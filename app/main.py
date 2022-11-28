3064cf946d0feb4b0d6dd36ed60ed3aef6578ee2
Tooraj Taraz <tooraj.info@gmail.com>
2022-11-28 16:59:40 +0330

    adding debug

--- app/main.py
+++ app/main.py
@@ -12,6 +12,7 @@ app.include_router(products_route, prefix="/api/products", tags=["products"])
 
 @app.middleware("http")
 async def add_process_time_header(request: Request, call_next):
+    print(f'is auth {request.is_authenticated}')
     if hasattr(request, 'is_authenticated') and request.is_authenticated:
         response = await call_next(request)
         return response
