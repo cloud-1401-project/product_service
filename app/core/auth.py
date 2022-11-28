05011d5a7a07a46431dd43536f4407c8f722e56f
Tooraj Taraz <tooraj.info@gmail.com>
2022-11-28 16:27:18 +0330

    adding debug

--- app/core/auth.py
+++ app/core/auth.py
@@ -6,6 +6,6 @@ def has_access(method, url, token):
     try:
         token = token.split('Bearer')[1].replace(" ", "").replace("\t", "")
         print(f'token is {token}')
-        return stub.HasAccess(auth_pb2.Resource(path=url, method=method, jwt=token)).has_access
+        return stub.HasAccess(auth_pb2.Resource(path=str(url), method=str(method), jwt=token)).has_access
     except Exception:
         return False
