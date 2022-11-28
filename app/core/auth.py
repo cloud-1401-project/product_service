12189439a562af677f52230da42c55230719a4a2
Tooraj Taraz <tooraj.info@gmail.com>
2022-11-28 17:08:33 +0330

    adding debug

--- app/core/auth.py
+++ app/core/auth.py
@@ -9,7 +9,7 @@ def has_access(method, url, token, req):
         print(f'url is {url}, {method}')
         access = stub.HasAccess(auth_pb2.Resource(path=str(url), method=str(method), jwt=token)).has_access
         print(f'access is {access}')
-        req.is_authenticated = access
+        setattr(req, 'is_authenticated', access)
         return access
     except Exception as e:
         print(f'exception is {e}')
