f6f93feb51b0623522d8de3f9ff3bb941ebe3485
Tooraj Taraz <tooraj.info@gmail.com>
2022-11-28 17:44:08 +0330

    fix extra arg

--- app/core/auth.py
+++ app/core/auth.py
@@ -2,7 +2,7 @@ from config import stub
 from config import auth_pb2
 
 
-def has_access(method, url, token, req):
+def has_access(method, url, token):
     try:
         token = token.split('Bearer')[1].replace(" ", "").replace("\t", "")
         access = stub.HasAccess(auth_pb2.Resource(path=str(url), method=str(method), jwt=token)).has_access
